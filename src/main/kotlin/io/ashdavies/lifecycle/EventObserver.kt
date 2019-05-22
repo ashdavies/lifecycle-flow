9e90ab6b99b52fa1524db83a9a9c421c69e6c8f7
Ash Davies <ashdavies@users.noreply.github.com>
2019-05-22 16:29:45 +0200

    Change event observer to interface implementation

--- src/main/kotlin/io/ashdavies/lifecycle/EventObserver.kt
+++ src/main/kotlin/io/ashdavies/lifecycle/EventObserver.kt
@@ -3,14 +3,14 @@ package io.ashdavies.lifecycle
 import androidx.lifecycle.Observer
 import io.ashdavies.architecture.Event
 
-abstract class EventObserver<T> : Observer<Event<T>> {
+interface EventObserver<T> : Observer<Event<T>> {
 
-  final override fun onChanged(it: Event<T>?) {
+  override fun onChanged(it: Event<T>?) {
     val content: T? = it?.content
     if (content != null) {
       onValue(content)
     }
   }
 
-  abstract fun onValue(it: T)
+  fun onValue(it: T)
 }
